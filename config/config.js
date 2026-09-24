3e5e479d81831b8b91ad5a4082b1d9d21929bac6
Q1nzh <qzhcjnb@gmail.com>
2026-09-24 22:22:02 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 /* 修改本文件即可切换 CloudDrive 读取的公开 GitHub 仓库与下载线路。切勿放入 Token。 */
 const CONFIG = {
   github: {
-    owner: "YOUR_GITHUB_USERNAME",
-    repo: "YOUR_REPOSITORY",
+    owner: "qzhcjnb",
+    repo: "cloud",
     branch: "main"
   },
   downloadProxies: [
